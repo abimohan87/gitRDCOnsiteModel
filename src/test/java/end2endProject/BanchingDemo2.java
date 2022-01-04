4cf47b06b195658d1311dd16ba84b8d1e68ded64
Abishek Mohan <abimohan87@gmail.com>
2022-01-04 15:42:02 +0530

    Test case added from Onsite team to the BanchingDemo2.java

--- src/test/java/end2endProject/BanchingDemo2.java
+++ src/test/java/end2endProject/BanchingDemo2.java
@@ -1,9 +1,17 @@
 package end2endProject;
 
+import org.testng.annotations.Test;
+
 public class BanchingDemo2 {
 
 	@Test
 	public void rdcTestAdd() {
+		
+	}
+	
+	@Test
+	public void onsiteTestAdd()
+	{
 		
 	}
 }
