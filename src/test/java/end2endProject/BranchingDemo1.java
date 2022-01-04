36a50c366bfdef1342fe966ddc73da3808b35b4e
Abishek Mohan <abimohan87@gmail.com>
2022-01-04 16:17:22 +0530

    Changes made to development branch from Onsite

--- src/test/java/end2endProject/BranchingDemo1.java
+++ src/test/java/end2endProject/BranchingDemo1.java
@@ -1,9 +1,16 @@
 package end2endProject;
 
+import org.testng.annotations.Test;
+
 public class BranchingDemo1 {
 
 	@Test
 	public void demoBranchTestRDC1() {
+		
+	}
+	
+	@Test
+	public void demoBranchTestOnsite() {
 		
 	}
 }
